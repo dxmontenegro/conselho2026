93e3433c15c8853cfe38346c330a82de95986e5b
dxmontenegro <monte.eng@gmail.com>
2026-04-21 19:15:14 -0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
 
     const resposta = await fetch(
-      "https://hinaavyyhoqcdycutqte.supabase.co/rest/v1/conselho_classe?select=*&limit=1",
+      "https://hinaavyyhoqcdycutqte.supabase.co/rest/v1/conselho_classes?select=*&limit=1",
       {
         headers: {
           apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpbmFhdnl5aG9xY2R5Y3V0cXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNTk1OTYsImV4cCI6MjA4NzYzNTU5Nn0.ZxWugRhPj8-2nf3GrolGmwgIWlteHz4r0tkonPZiBMs"
